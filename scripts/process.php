fc5a33ef16363cc95d8f189660ad0d6eadfb4b22
tyler@shetrompf.com <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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>
2023-12-03 22:16:46 -0500

    began adding database connectivity

--- scripts/process.php
+++ scripts/process.php
@@ -1,7 +1,15 @@
 <?php
+
 $errors = [];
 $data = [];
 
+//Connect to Postgresql database
+$surestore_db = pg_connect("host=localhost dbname=SureStore user=postgres password=97DnXjPQSUu$925atBo!9WZuAf@7aaWQ");
+
+pg_close($connect);
+// Initialize arrays for error messages and data
+
+// Return error text if fields are empty
 if (empty($_POST['username'])) {
     $errors['username'] = 'Username is required.';
 }
@@ -19,3 +27,5 @@ if (!empty($errors)) {
 }
 
 echo json_encode($data);
+
+?>
