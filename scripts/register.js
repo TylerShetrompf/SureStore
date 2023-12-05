6d3a4711cb6510677e1eb6378609857cf5d32f59
tyler@shetrompf.com <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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>
2023-12-05 15:39:14 -0500

    reworked registration form, registration to database is now functional

--- scripts/register.js
+++ scripts/register.js
@@ -1,9 +1,33 @@
 // JavaScript Document to handle auth form
 $(document).ready(function () {
-	$("#Register").submit(function (event) {
-		event.preventDefault();
-		$.ajax({}).done(function () {
-			$("#App").innerHTML = "test success";
+	$(".regButton").click(function () {
+		$.get('/snippets/registration.html', function(data) {
+			$("#App").html(data);
+			console.log("regButtonPushed");
 		})
-	})
-})
+	});
+});
+
+$(document).ready(function (){
+	$("body").on("submit", "#RegisterForm", function(event) {
+		event.preventDefault();
+		var formData ={
+			firstname: $("#firstname").val(),
+			lastname: $("#lastname").val(),
+			email: $("#email").val(),
+			password: $("#password").val(),
+		}
+		console.log("formDataSubmitted");
+		console.log(formData);
+		console.log(firstname);
+		$.ajax({
+			type: "POST",
+			url: "/scripts/register.php",
+			data: formData,
+			dataType: "json",
+			encode: true,
+		}).done(function (data) {
+			console.log(data);
+		});
+	});
+});
