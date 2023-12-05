f65fab3044367759860488562e3b67244f654488
tyler@shetrompf.com <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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>
2023-12-05 16:46:56 -0500

    Removed extraneous console logging, added code for handling registration username errors

--- scripts/register.js
+++ scripts/register.js
@@ -1,25 +1,25 @@
 // JavaScript Document to handle auth form
 $(document).ready(function () {
 	$(".regButton").click(function () {
+		
 		$.get('/snippets/registration.html', function(data) {
 			$("#App").html(data);
-			console.log("regButtonPushed");
 		})
 	});
 });
 
 $(document).ready(function (){
 	$("body").on("submit", "#RegisterForm", function(event) {
+		
 		event.preventDefault();
+		
 		var formData ={
 			firstname: $("#firstname").val(),
 			lastname: $("#lastname").val(),
 			email: $("#email").val(),
 			password: $("#password").val(),
 		}
-		console.log("formDataSubmitted");
-		console.log(formData);
-		console.log(firstname);
+		
 		$.ajax({
 			type: "POST",
 			url: "/scripts/register.php",
@@ -27,7 +27,16 @@ $(document).ready(function (){
 			dataType: "json",
 			encode: true,
 		}).done(function (data) {
-			console.log(data);
+
+			if (!data.success) {
+				if (data.errors.username) {
+					$("#FormEmailAddress").addClass("has-error");
+					$("#FormEmailAddress").append(
+						'<div class="help-block">' + data.errors.username + "</div>"
+					);
+				}
+			}
+			
 		});
 	});
 });
