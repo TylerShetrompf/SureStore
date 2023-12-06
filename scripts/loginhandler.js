7ea8904f39ff5445bfbe7f18e40ce16c460f0d14
tyler@shetrompf.com <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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>
2023-12-06 14:11:10 -0500

    added comments, test for html swap on successful login, began building out session cookie functionality

--- scripts/loginhandler.js
+++ scripts/loginhandler.js
@@ -1,11 +1,27 @@
-// JavaScript Document to handle auth form
+// JavaScript to handle login
 $(document).ready(function () {
+	
+	// Check for sessionid cookie
+	const sessionidValue = document.cookie
+		.split("; ")
+		.find((row) => row.startsWith("sessionid="))
+		?.split("=")[1];
+		console.log(sessionidValue);
+	
+	// Check for userid cookie
+	const useridValue = document.cookie
+		.split("; ")
+		.find((row) => row.startsWith("userid="))
+		?.split("=")[1];
+	console.log(useridValue);
+	// Grab formdata when login form is submitted
 	$("#LoginForm").submit(function (event) {
 		var formData ={
 			username: $("#username").val(),
 			password: $("#password").val(),
 		};
-
+		
+		// ajax request for login processing script
 		$.ajax({
 			type: "POST",
 			url: "/scripts/process.php",
@@ -14,6 +30,7 @@ $(document).ready(function () {
 			encode: true,
 		}).done(function (data){
 			
+			// Check for errors
 			if (!data.success) {
 				
 				if (data.errors.password) {
@@ -27,6 +44,13 @@ $(document).ready(function () {
 						'<div class="help-block">' + data.errors.activation + "</div>"
 					);
 				}
+			// If no errors, continue
+			} else {
+				
+				// Load main menu
+				$.get('/snippets/mainmenu.html', function(newHTMLdata) {
+					$("#App").html(newHTMLdata);
+				})
 			}
 		});
 		
