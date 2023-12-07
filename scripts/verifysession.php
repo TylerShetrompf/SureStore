0e0fe152bba8fe837458510ff406822a8f5e5500
tyler@shetrompf.com <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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>
2023-12-06 20:47:39 -0500

    initial commit, this is functional

---
+++ scripts/verifysession.php
@@ -0,0 +1,28 @@
+<?php
+
+// Initialize arrays for error messages and data
+$data = [];
+
+$surestore_db = pg_pconnect("host=localhost dbname=SureStore user=postgres password=97DnXjPQSUu$925atBo!9WZuAf@7aaWQ");
+
+$userid = $_POST['userid'];
+$sessionid = $_POST['sessionid'];
+$query = pg_query_params($surestore_db, 'SELECT userid, sessionid FROM sureusers WHERE userid = $1', array($userid));
+
+$results = pg_fetch_assoc($query);
+
+$useridresult = $results["userid"];
+echo($useridresult);
+$sessionidresult = $results["sessionid"];
+
+pg_close($surestore_db);
+
+if (empty($useridresult)) {
+	$data["success"] = false;
+} elseif ($sessionidresult != $sessionid) {
+	$data["success"] = false;
+} else {
+	$data["success"] = true;
+}
+
+echo json_encode($data);
