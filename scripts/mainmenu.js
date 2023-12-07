24de6eda0ed7d43a70d37c230086632c3c4ec265
tyler@shetrompf.com <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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>
2023-12-07 13:12:15 -0500

    added js to load in locator html when locator button is pushed

--- scripts/mainmenu.js
+++ scripts/mainmenu.js
@@ -1,1 +1,8 @@
 // JavaScript Document for handling the main menu
+$(document).ready(function () {
+	$("body").on("click", "#locButton", function() {
+		$.get('/snippets/locator.html', function(data) {
+			$("#App").html(data);
+		})
+	});
+});
