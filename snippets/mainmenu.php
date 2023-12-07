b562b9dabb3b89fb5c66fded18adcf2f6068be9a
tyler@shetrompf.com <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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>
2023-12-07 11:28:26 -0500

    initial commit

---
+++ snippets/mainmenu.php
@@ -0,0 +1,10 @@
+<?php
+	$surestore_db = pg_pconnect("host=localhost dbname=SureStore user=postgres password=97DnXjPQSUu$925atBo!9WZuAf@7aaWQ");
+	pg_close($surestore_db);
+?>
+
+<div class="FormRow">
+	<form id="vaultlocButton" class="mainButton">
+		<button type=button class="stylish-button">Register</button>
+	</form>
+</div>
