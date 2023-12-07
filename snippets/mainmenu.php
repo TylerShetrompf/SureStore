8d0557243198730f9c5fcd7e58d314a664e30b98
tyler@shetrompf.com <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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>
2023-12-07 13:14:10 -0500

    added reverifysession inclusion, updated locatorbutton to be labeled and function properly

--- snippets/mainmenu.php
+++ snippets/mainmenu.php
@@ -1,10 +1,9 @@
 <?php
-	include '/var/www/html/scripts/connectdb.php';
-	pg_close($surestore_db);
+	// PHP to check session
+	include '/var/www/html/scripts/reverifysession.php';
 ?>
-
-<div class="FormRow">
-	<form id="vaultlocButton" class="mainButton">
-		<button type=button class="stylish-button">Register</button>
+<div class="locatorButton">
+	<form id="locatorButton" class="mainButton">
+		<button type=button id="locButton" class="stylish-button">Locator</button>
 	</form>
 </div>
