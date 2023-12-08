f57bb1701bd695d083fa1393af892f1f5a6e303c
tyler@shetrompf.com <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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>
2023-12-08 13:50:05 -0500

    updated query to return joined tables with all needed information

--- scripts/vaultsearch.php
+++ scripts/vaultsearch.php
@@ -1,10 +1,18 @@
 <?php
+// Include db connection
 include '/var/www/html/scripts/connectdb.php';
-$userinput = $_POST['query'];
-$dbquery = pg_query_params($surestoredb, "SELECT * from sureorders WHERE orderid LIKE $1 OR ordercust LIKE $1", array($us erinput));
 
+// Get user input from post, append %
+$userinput = $_POST['query']."%";
+
+// Query
+$dbquery = pg_query_params($surestore_db, "select surecustomer.custfirst, surecustomer.custlast, sureorders.orderid, sureitems.itemvault from sureorders inner join surecustomer on sureorders.ordercust=surecustomer.custid inner join sureitems on sureorders.orderid=sureitems.itemorder WHERE sureorders.orderid like $1 OR surecustomer.custfirst like $1 OR surecustomer.custlast like $1 OR sureorders.orderid like $1", array($userinput));
+
+// Save result to associative array
 $dbresult = pg_fetch_assoc($dbquery);
 
-echo json_encode($searchresult);
+// Echo results
+echo json_encode($dbresult);
 
+// Close DB connection
 pg_close($surestore_db);
