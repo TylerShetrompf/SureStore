18ac8e0d7856b7a48e2f949ecf93c95557355816
tyler@shetrompf.com <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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>
2023-12-08 00:44:26 -0500

    corrected errors preventing locbutton event handler from functioning. Added unfinished code for event handler for vault search bar

--- scripts/mainmenu.js
+++ scripts/mainmenu.js
@@ -1,8 +1,25 @@
 // JavaScript Document for handling the main menu
 $(document).ready(function () {
-	$("body").on("click", "#locButton", function() {
+	$("body").on("click", "#locButton", function(event) {
+		event.preventDefault();
 		$.get('/snippets/locator.html', function(data) {
 			$("#App").html(data);
+		})
+	});
+});
+
+
+$(document).ready(function () {
+	$("body").on("keyup", "#vaultsearchform", function () {
+		var query = $("#search").val();
+		$.ajax({
+			type: "POST",
+			url: "/scripts/vaultsearch.php",
+			data: query,
+			dataType: "json",
+			encode: true,
+		}).done(function (data){
+			console.log(data);
 		})
 	});
 });
