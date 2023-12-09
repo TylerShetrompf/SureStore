a40100b47ea3b9e98ad278278d5296bebf6ba451
tyler@shetrompf.com <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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>
2023-12-09 00:00:13 -0500

    added buttons for future pages (currently do nothing)

--- snippets/mainmenu.php
+++ snippets/mainmenu.php
@@ -2,8 +2,14 @@
 	// PHP to check session
 	include '/var/www/html/scripts/reverifysession.php';
 ?>
-<div class="locatorButton">
+<div class="mainMenu">
 	<form id="locatorButton" class="mainButton">
 		<button type=button id="locButton" class="stylish-button">Locator</button>
+	</form>
+	<form id="maintbuttonform" class="mainButton">
+		<button type=button id="maintButton" class="stylish-button">Maintenance</button>
+	</form>
+	<form id="adminbuttonform" class="mainButton">
+		<button type=button id="adminButton" class="stylish-button">Admin</button>
 	</form>
 </div>
