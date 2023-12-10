b7efdf06743a60826895834cc6121e350745f24b
tyler@shetrompf.com <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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>
2023-12-10 01:31:30 -0500

    made functional backend search

--- scripts/vaultsearch.php
+++ scripts/vaultsearch.php
@@ -2,17 +2,31 @@
 // Include db connection
 include '/var/www/html/scripts/connectdb.php';
 
+// Initialize data and results arrays
+$data = [];
+$results = [];
+
 // Get user input from post, append %
-$userinput = $_POST['query']."%";
+$userinput = $_POST['term']."%";
 
 // Query
 $dbquery = pg_query_params($surestore_db, "select surecustomer.custfirst, surecustomer.custlast, sureorders.orderid, sureitems.itemvault from sureorders inner join surecustomer on sureorders.ordercust=surecustomer.custid inner join sureitems on sureorders.orderid=sureitems.itemorder WHERE sureorders.orderid like $1 OR surecustomer.custfirst like $1 OR surecustomer.custlast like $1 OR sureorders.orderid like $1", array($userinput));
 
-// Save result to associative array
-$dbresult = pg_fetch_assoc($dbquery);
+// Initialize ID variable
+$id = 1;
 
-// Echo results
-echo json_encode($dbresult);
+// Save result to associative array, iterate through array, assign results
+while ($dbresult = pg_fetch_assoc($dbquery)) {
+	$entry = [];
+	$entry["id"] = $id;
+	$entry["text"] = "Reg: ".$dbresult["orderid"]." Customer: ".$dbresult["custfirst"]." ".$dbresult["custlast"]." Vault: ".$dbresult["itemvault"];
+	array_push($results, $entry);
+	$id++;
+}
+$data["results"] = $results;
+
+// Echo back results
+echo json_encode($data);
 
 // Close DB connection
 pg_close($surestore_db);
