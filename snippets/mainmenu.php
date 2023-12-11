edb4ebdd9880a6ef0b32168ab1a63e1de2b36927
tyler@shetrompf.com <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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>
2023-12-10 22:49:39 -0500

    added top navigation bar

--- snippets/mainmenu.php
+++ snippets/mainmenu.php
@@ -2,6 +2,12 @@
 	// PHP to check session
 	include '/var/www/html/scripts/reverifysession.php';
 ?>
+
+<div class="topnav">
+	<a href="#Home">Home</a>
+	<a href="#Locator">Locator</a>
+	<a href="#news">Logout</a>
+</div>
 <div class="mainMenu">
 	<form id="locatorButton" class="mainButton">
 		<button type=button id="locButton" class="stylish-button">Locator</button>
