8bbc373e613c1285ed2f905d80484b4514c18120
tyler@shetrompf.com <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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>
2023-12-11 12:17:16 -0500

    added and made functional listeners for homenav, admin, and maintenance buttons

--- scripts/mainmenu.js
+++ scripts/mainmenu.js
@@ -1,12 +1,37 @@
 // JavaScript Document for handling the main menu
 $(document).ready(function () {
+	
+	// Locator button
 	$("body").on("click", "#locButton", function(event) {
 		event.preventDefault();
-		$.get('/snippets/locator.html', function(data) {
+		$.get('/snippets/locator.php', function(data) {
 			$("#App").html(data);
 			
 			// Call initialize selct2 function so that select 2 registers box on load
 			initializeSelect2();
+		})
+	});
+	
+	// Maintenance button
+	$("body").on("click", "#maintButton", function(event) {
+		event.preventDefault();
+		$.get('/snippets/maintenance.php', function(data) {
+			$("#App").html(data);
+		})
+	});
+	
+	$("body").on("click", "#adminButton", function(event) {
+		event.preventDefault();
+		$.get('/snippets/admin.php', function(data) {
+			$("#App").html(data);
+		})
+	});
+	
+	// Home nav button
+	$("body").on("click", "#homenav", function(event) {
+		event.preventDefault();
+		$.get('/snippets/mainmenu.php', function(data) {
+			$("#App").html(data);
 		})
 	});
 });
