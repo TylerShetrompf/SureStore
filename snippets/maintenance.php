816886d059db536d1811dd0509d1910c97e7bd82
tyler@shetrompf.com <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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>
2023-12-11 12:19:28 -0500

    initial commit, placeholder for maintenance snippet

---
+++ snippets/maintenance.php
@@ -0,0 +1,11 @@
+<?php
+	// PHP to check session
+	include '/var/www/html/scripts/reverifysession.php';
+?>
+
+<div class="topnav">
+	<a href="#Home" class="navbutton" id="homenav">Home</a>
+	<a href="#Logout" class="navbutton" id="logoutnav">Logout</a>
+</div>
+
+<h1> This is the maintenance page. </h1>
