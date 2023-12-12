b07585db28473720aabfd073300417049e388fad
tyler@shetrompf.com <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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>
2023-12-12 13:01:15 -0500

    removed navbar from mainmenu, it has its own snippet now.

--- snippets/mainmenu.php
+++ snippets/mainmenu.php
@@ -2,11 +2,6 @@
 	// PHP to check session
 	include '/var/www/html/scripts/reverifysession.php';
 ?>
-
-<div class="topnav">
-	<a href="#Home" class="navbutton" id="homenav">Home</a>
-	<a href="#Logout" class="navbutton" id="logoutnav">Logout</a>
-</div>
 <div class="mainMenu">
 	<form id="locatorButton" class="mainButton">
 		<button type=button id="locButton" class="stylish-button">Locator</button>
