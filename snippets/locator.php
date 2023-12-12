bbf8b436eb1e4ac6e3639a2b35df7b05cd2faef7
tyler@shetrompf.com <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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>
2023-12-12 13:48:17 -0500

    removed navbar, now in its own snippet

--- snippets/locator.php
+++ snippets/locator.php
@@ -3,10 +3,6 @@
 	include '/var/www/html/scripts/reverifysession.php';
 ?>
 
-<div class="topnav">
-	<a href="#Home" class="navbutton" id="homenav">Home</a>
-	<a href="#Logout" class="navbutton" id="logoutnav">Logout</a>
-</div>
 <select class="vaultSearch" name="search" id="search">
 </select>
 <h2> OR </h2>
