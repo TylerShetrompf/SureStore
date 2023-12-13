6af4427dedf23d2ce5fc81a70761342f54d61b78
tyler@shetrompf.com <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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>
2023-12-13 15:39:43 -0500

    removed navbar, as it is no longer loaded page by page

--- snippets/maintenance.php
+++ snippets/maintenance.php
@@ -2,10 +2,4 @@
 	// PHP to check session
 	include '/var/www/html/scripts/reverifysession.php';
 ?>
-
-<div class="topnav">
-	<a href="#Home" class="navbutton" id="homenav">Home</a>
-	<a href="#Logout" class="navbutton" id="logoutnav">Logout</a>
-</div>
-
 <h1> This is the maintenance page. </h1>
