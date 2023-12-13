2e07da9f763f1bb59eb36784fd1730b125eafe2a
tyler@shetrompf.com <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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>
2023-12-13 15:39:17 -0500

    initial commit, contains initializeselect2 function previously found in mainmenu.js

---
+++ scripts/vaultsearch.js
@@ -0,0 +1,54 @@
+// JavaScript Document to handle vault search
+
+// Select2 stuff
+function initializeSelect2(){
+	$('.vaultSearch').select2({
+		placeholder: "Search by registration, customer name, or vault...",
+		ajax: {
+			type: "POST",
+			url: '/scripts/vaultsearch.php',
+			data: function(term){
+				return term;
+			},
+			dataType: "json",
+			encode: true,
+			processResults: function (data) {
+				return data;
+			}
+		}
+	});
+	
+	// Grab selected content, format
+	$('.vaultSearch').on("change", function(event) {
+		event.preventDefault();
+		
+		// Set content variable
+		var content = $(".select2-selection__rendered").text();
+		
+		// Narrow content down to array
+		content = content.replace("Reg: ", "");
+		content = content.replace(" Customer: ",",");
+		content = content.replace(" Vault: ",",");
+		content = content.replace(" ",",");
+		content = content.split(",");
+		
+		// Assign values to associative array
+		var formData = {
+			orderid: content[0],
+		};
+		$.ajax({
+			type: "POST",
+			url: "/scripts/vaultinfo.php",
+			data: formData,
+			dataType: "json",
+			encode: true,
+		}).done(function (returndata){
+			$.get('/snippets/vaultinfoleft.html', function(data) {
+				$(".leftside").html(data);
+			})
+			$.get('/snippets/vaultinforight.html', function(data) {
+				$(".rightside").html(data);
+			})
+		});
+	});
+}
