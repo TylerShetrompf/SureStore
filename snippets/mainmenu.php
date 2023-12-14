2602e7eea8901d02d82e2ab7e226dfa10d4115fc
tyler@shetrompf.com <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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>
2023-12-14 14:40:04 -0500

    added comments, added column divs

--- snippets/mainmenu.php
+++ snippets/mainmenu.php
@@ -2,12 +2,28 @@
 	// PHP to check session
 	include '/var/www/html/scripts/reverifysession.php';
 ?>
-<div class="d-grid gap-2 my-2" id="locButton">
-	<button type="button" class="btn btn-success btn-lg btn-block">Locator</button>
+
+<!-- 3 Columns in Main Row -->
+			
+<!-- Left row -->
+<div class="col-sm-4" id="left"></div>
+
+<!-- Middle row -->
+<div class="col-sm-4" id="middle">
+	
+	<div class="d-grid gap-2 my-2" id="locButton">
+		<button type="button" class="btn btn-success btn-lg btn-block">Locator</button>
+	</div>
+	
+	<div class="d-grid gap-2 my-2" id="maintButton">
+		<button type="button" class="btn btn-success btn-lg btn-block">Maintenance</button>
+	</div>
+	
+	<div class="d-grid gap-2 my-2" id="adminButton">
+		<button type="button" class="btn btn-success btn-lg btn-block">Admin</button>
+	</div>
+	
 </div>
-<div class="d-grid gap-2 my-2" id="maintButton">
-	<button type="button" class="btn btn-success btn-lg btn-block">Maintenance</button>
-</div>
-<div class="d-grid gap-2 my-2" id="adminButton">
-	<button type="button" class="btn btn-success btn-lg btn-block">Admin</button>
-</div>
+
+<!-- Right row -->
+<div class="col-sm-4" id="right"></div>
