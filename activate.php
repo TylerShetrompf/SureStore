7277ef0637ff839a504844475b2cacaec16da6f8
tyler@shetrompf.com <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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>
2023-12-14 01:58:00 -0500

    modified for bootstrap compat

--- activate.php
+++ activate.php
@@ -5,24 +5,10 @@
 <meta charset="utf-8">
 <title>Account Activated</title>
 <link rel="shortcut icon" href="https://www.surestore.store/images/favicon.ico">
-<link rel="stylesheet" href="/style/style.css">
-<style>
-	body {
-		align-content: center;
-	}
-	#activationerror {
-		background-color: #000000;
-	}
-
-	h1 {
-		padding: 12px 20px;
-		display: inline-block;
-		border: 1px solid #ccc;
-		border-radius: 20px;
-		box-sizing: border-box;
-		background-color: #5781A4;
-	}	
-</style>
+<!-- Include Bootstrap -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.min.js" integrity="sha512-WW8/jxkELe2CAiE4LvQfwm1rajOS8PHasCCx+knHG0gBHt8EXxS6T6tJRTGuDQVnluuAvMxWF4j8SNFDKceLFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>	
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="/style/style2.css">
 </head>
 
 <body>
@@ -41,19 +27,9 @@
 		// Validate query
 		if($checkuserresults){
 			$actuserquery = pg_query_params($surestore_db, "UPDATE sureusers SET activated = 'TRUE' WHERE userid = $1 AND actcode = $2", array($userid, $actcode));
-			echo('<img src="images/logoSmol.png" alt="logo" size="50%">');
-			echo('<h1>Congratulations! ');
-			echo('Your SureStore account is now activated. ');
-			echo('Please click the button below to return to the login page:</h1>');
-			echo('<form id="returntologin" action="https://surestore.store">');
-			echo('<button type="submit" class="stylish-button"/>Go to login</form>');
+			echo('<div class="container"><div class="row"><div class="col-sm-3"></div><div class="col-sm-6" id="body"><img class="mx-auto d-block" src="images/logoSmol.png" alt="SureStore Logo"><div class="bg-light rounded-3"><h1>Account Activated</h1><p>Your SureStore account is now activated!</p></div><div class="d-grid gap-2 my-2"><a href="https://surestore.store" class="btn btn-success btn-large btn-block">Return to login</a></div></div><div class="col-sm-3"></div><footer class="footer"><div class="container-fluid"><span class="text-white">&copy; SureStore.store</span></div></footer></div>');
 		} else {
-			echo('<img src="images/logoSmol.png" alt="logo" size="50%">');
-			echo('<div class="help-block">');
-			echo('<h1 id="activationerror"> Activation link invalid. Please try again or contact your server administrator. </h1>');
-			echo('</div>');
-			echo('<form id="returntologin" action="https://surestore.store">');
-			echo('<button type="submit" class="stylish-button"/>Go to login</form>');
+			echo('<div class="container"><div class="row"><div class="col-sm-3"></div><div class="col-sm-6" id="body"><img class="mx-auto d-block" src="images/logoSmol.png" alt="SureStore Logo"><div class="bg-danger rounded-3"><h1>Account Activation Failed :(</h1><p>Activation link is invalid.</p></div><div class="d-grid gap-2 my-2"><a href="https://surestore.store" class="btn btn-danger btn-large btn-block">Return to login</a></div></div><div class="col-sm-3"></div><footer class="footer"><div class="container-fluid"><span class="text-white">&copy; SureStore.store</span></div></footer></div>');
 		}
 		pg_close($surestore_db);
 	?>
