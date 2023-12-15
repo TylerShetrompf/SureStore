d6993b5e57ec4e266b1a9f21eaca898e93377f6c
tyler@shetrompf.com <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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>
2023-12-15 17:37:04 -0500

    added code for item add

--- scripts/editorscripts/itemadd.php
+++ scripts/editorscripts/itemadd.php
@@ -1,10 +1,32 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+// Include db connection
+include '/var/www/html/scripts/connectdb.php';
+
+$itemdesc = $_POST["itemdesc"];
+$itemvault = $_POST["itemvault"];
+$itemloose = $_POST["itemloose"];
+$itemorder = $_POST["itemorder"];
+
+// Turn vaulter name into vaulterid 
+$itemvaultername = $_POST["itemvaulter"];
+$vaulternamearray = explode(" ", $itemvaultername);
+$vaulterfirst = $vaulternamearray[0];
+$vaulterlast = $vaulternamearray[1];
+$vaulteridquery = pg_query_params($surestore_db, "select * from surevaulters where vaulterfirst = $1 AND vaulterlast = $2", array($vaulterfirst, $vaulterlast));
+$vaulteridresult = pg_fetch_assoc($vaulteridquery);
+$itemvaulter = $vaulteridresult["vaulterid"];
+
+if ($itemvault){
+	$itemcreatequery = pg_query_params($surestore_db, "INSERT INTO sureitems (itemdesc, itemvault, itemorder, itemvaulter) VALUES ($1, $2, $3, $4) RETURNING *;", array($itemdesc, $itemvault, $itemorder, $itemvaulter));
+	$createqueryresult = pg_fetch_assoc($itemcreatequery);
+	$createqueryresult["itemvaulter"] = $itemvaultername;
+	echo json_encode($createqueryresult);
+}
+
+if ($itemloose){
+	$itemcreatequery = pg_query_params($surestore_db, "INSERT INTO sureitems (itemdesc, itemloose, itemorder, itemvaulter) VALUES ($1, $2, $3, $4) RETURNING *;", array($itemdesc, $itemloose, $itemorder, $itemvaulter));
+	$createqueryresult = pg_fetch_assoc($itemcreatequery);
+	$createqueryresult["itemvaulter"] = $itemvaultername;
+	echo json_encode($createqueryresult);
+}
