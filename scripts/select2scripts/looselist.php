0867bcb856b5d8de0905eddd15f04f9ffdca31f1
tyler@shetrompf.com <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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>
2023-12-15 16:03:32 -0500

    initial commit, php script for select 2 looselist

---
+++ scripts/select2scripts/looselist.php
@@ -0,0 +1,29 @@
+<?php
+// Include db connection
+include '/var/www/html/scripts/connectdb.php';
+
+// Initialize data and results arrays
+$data = [];
+$results = [];
+
+// Get user input from post, append %
+$userinput = $_POST['term']."%";
+
+// Query
+$dbquery = pg_query_params($surestore_db, "select * from sureloose where looseid  like $1", array($userinput));
+
+// Initialize ID variable
+$id = 1;
+
+// Save result to associative array, iterate through array, assign results
+while ($dbresult = pg_fetch_assoc($dbquery)) {
+	$entry = [];
+	$entry["id"] = $id;
+	$entry["text"] = $dbresult["looseid"];
+	array_push($results, $entry);
+	$id++;
+}
+$data["results"] = $results;
+
+// Echo back results
+echo json_encode($data);
