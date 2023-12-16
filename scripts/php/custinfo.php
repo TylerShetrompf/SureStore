7ca7ebf6257b82ec6c73c3075c559acfc0cd6f29
tyler@shetrompf.com <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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>
2023-12-16 17:51:44 -0500

    removed placeholder text, added code for select2 searching

--- scripts/php/custinfo.php
+++ scripts/php/custinfo.php
@@ -1,10 +1,32 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+// Include db connection
+include '/var/www/html/scripts/php/connectdb.php';
 
-<body>
-</body>
-</html>
+$data = [];
+$results = [];
+
+// Get user input from post, append %
+$userinput = $_POST['term']."%";
+
+// Query
+$dbquery = pg_query_params($surestore_db, "select * from surecustomer where LOWER(surecustomer.custfirst) like LOWER($1) or LOWER(surecustomer.custlast) like LOWER($1) or LOWER(surecustomer.custbusiness) like LOWER($1)", array($userinput));
+
+// Initialize ID variable
+$id = 1;
+
+while ($dbresult = pg_fetch_assoc($dbquery)) {
+	
+	$entry = [];
+	$entry["id"] = $id;
+	$entry["text"] = "Customer: ".$dbresult["custfirst"]." ".$dbresult["custlast"]." Business: ".$dbresult["custbusiness"];
+	array_push($results, $entry);
+	$id++;
+}
+
+$data["results"] = $results;
+
+// Echo back results
+echo json_encode($data);
+
+// Close DB connection
+pg_close($surestore_db);
