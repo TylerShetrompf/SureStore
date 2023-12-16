f7fed13f67274ca5b1ae2d3c9ddf0df9a18848f9
tyler@shetrompf.com <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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>
2023-12-16 16:51:10 -0500

    incomplete ajax call to reginfo.php, jquery for tn masking on vaultinfo middle

--- scripts/js/reginfo.js
+++ scripts/js/reginfo.js
@@ -1,15 +1,73 @@
-// JavaScript Document
+// JavaScript Document for reginfo page
 function fillreginfo(orderid){
 	var formData ={
 		orderid: orderid,
 	};
 	$.ajax({
-		url: "/scripts/php/reginfo.php"
-		type:"POST",
+		url: "/scripts/php/reginfo.php",
+		type: "POST",
 		data: formData,
 		dataType: "json",
 		encode: true
 	}).done(function (data){
 		
+	});
+	
+	// Phone number masking
+	$('#cust-tn')
+
+	.keydown(function (e) {
+		var key = e.which || e.charCode || e.keyCode || 0;
+		$phone = $(this);
+
+    // Don't let them remove the starting '('
+    if ($phone.val().length === 1 && (key === 8 || key === 46)) {
+			$phone.val('('); 
+      return false;
+		} 
+    // Reset if they highlight and type over first char.
+    else if ($phone.val().charAt(0) !== '(') {
+			$phone.val('('+String.fromCharCode(e.keyCode)+''); 
+		}
+
+		// Auto-format- do not expose the mask as the user begins to type
+		if (key !== 8 && key !== 9) {
+			if ($phone.val().length === 4) {
+				$phone.val($phone.val() + ')');
+			}
+			if ($phone.val().length === 5) {
+				$phone.val($phone.val() + ' ');
+			}			
+			if ($phone.val().length === 9) {
+				$phone.val($phone.val() + '-');
+			}
+		}
+
+		// Allow numeric (and tab, backspace, delete) keys only
+		return (key == 8 || 
+				key == 9 ||
+				key == 46 ||
+				(key >= 48 && key <= 57) ||
+				(key >= 96 && key <= 105));	
+	})
+	
+	.bind('focus click', function () {
+		$phone = $(this);
+		
+		if ($phone.val().length === 0) {
+			$phone.val('(');
+		}
+		else {
+			var val = $phone.val();
+			$phone.val('').val(val); // Ensure cursor remains at the end
+		}
+	})
+	
+	.blur(function () {
+		$phone = $(this);
+		
+		if ($phone.val() === '(') {
+			$phone.val('');
+		}
 	});
 }
