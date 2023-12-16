56ca5f3c42fb812bddef02fd12c1622c680d7e1e
tyler@shetrompf.com <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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>
2023-12-16 14:23:15 -0500

    added div tags so that mainmenu.php can be loaded into #appcontainer tag instead of #rowmain tag

--- snippets/mainmenu.php
+++ snippets/mainmenu.php
@@ -3,27 +3,31 @@
 	include '/var/www/html/scripts/php/reverifysession.php';
 ?>
 
-<!-- 3 Columns in Main Row -->
-			
-<!-- Left row -->
-<div class="col-sm-4" id="left"></div>
+<div class="row" id="rowmain">
 
-<!-- Middle row -->
-<div class="col-sm-4" id="middle">
-	
-	<div class="d-grid gap-2 my-2" id="locButton">
-		<button type="button" class="btn btn-success btn-lg btn-block">Locator</button>
+	<!-- 3 Columns in Main Row -->
+
+	<!-- Left row -->
+	<div class="col-sm-4" id="left"></div>
+
+	<!-- Middle row -->
+	<div class="col-sm-4" id="middle">
+
+		<div class="d-grid gap-2 my-2" id="locButton">
+			<button type="button" class="btn btn-success btn-lg btn-block">Locator</button>
+		</div>
+
+		<div class="d-grid gap-2 my-2" id="maintButton">
+			<button type="button" class="btn btn-success btn-lg btn-block">Maintenance</button>
+		</div>
+
+		<div class="d-grid gap-2 my-2" id="adminButton">
+			<button type="button" class="btn btn-success btn-lg btn-block">Admin</button>
+		</div>
+
 	</div>
-	
-	<div class="d-grid gap-2 my-2" id="maintButton">
-		<button type="button" class="btn btn-success btn-lg btn-block">Maintenance</button>
-	</div>
-	
-	<div class="d-grid gap-2 my-2" id="adminButton">
-		<button type="button" class="btn btn-success btn-lg btn-block">Admin</button>
-	</div>
+
+	<!-- Right row -->
+	<div class="col-sm-4" id="right"></div>
 	
 </div>
-
-<!-- Right row -->
-<div class="col-sm-4" id="right"></div>
