5ba9d48af58917f6632b659b2ec46a05908b8234
tyler@shetrompf.com <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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>
2023-12-16 17:30:19 -0500

    removed placeholder code and added code to return the needed data

--- scripts/php/reginfo.php
+++ scripts/php/reginfo.php
@@ -1,10 +1,15 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+// Include db connection
+include '/var/www/html/scripts/php/connectdb.php';
+
+// Initialize data array
+$data = [];
+
+$orderid = $_POST["orderid"];
+
+$reginfoquery = pg_query_params($surestore_db, "select sureorders.orderid, sureorders.orderwh, sureorders.ordermil, sureorders.datein, sureorders.dateout, sureorders.weight, surecustomer.* from sureorders left join surecustomer on surecustomer.custid = sureorders.ordercust and sureorders.orderid=$1", array($orderid));
+
+$reginforesults = pg_fetch_assoc($reginfoquery);
+
+echo json_encode($reginforesults);
