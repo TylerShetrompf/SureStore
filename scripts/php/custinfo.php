61cb5241ece5cefb921921c88f173734c81edcad
tyler@shetrompf.com <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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>
2023-12-17 18:54:46 -0500

    added logic to handle filling cust form as well as handling custid select2 query

--- scripts/php/custinfo.php
+++ scripts/php/custinfo.php
@@ -5,28 +5,45 @@ include '/var/www/html/scripts/php/connectdb.php';
 $data = [];
 $results = [];
 
-// Get user input from post, append %
-$userinput = $_POST['term']."%";
-
-// Query
-$dbquery = pg_query_params($surestore_db, "select * from surecustomer where LOWER(surecustomer.custfirst) like LOWER($1) or LOWER(surecustomer.custlast) like LOWER($1) or LOWER(surecustomer.custbusiness) like LOWER($1)", array($userinput));
-
-// Initialize ID variable
-$id = 1;
-
-while ($dbresult = pg_fetch_assoc($dbquery)) {
+// if orderid was posted, collect info for selected order, else return info for select2 box
+if ($_POST["orderid"]){
+	// get orderid from post
+	$orderid = $_POST["orderid"];
 	
-	$entry = [];
-	$entry["id"] = $id;
-	$entry["text"] = "Customer: ".$dbresult["custfirst"]." ".$dbresult["custlast"]." Business: ".$dbresult["custbusiness"];
-	array_push($results, $entry);
-	$id++;
+	// Query
+	$dbquery = pg_query_params($surestore_db, "select surecustomer.* from sureorders left join surecustomer on surecustomer.custid = sureorders.ordercust where sureorders.orderid=$1", array($orderid));
+	
+	// assign results to assoc array
+	$results = pg_fetch_assoc($dbquery);
+	
+	// echo results as json
+	echo json_encode($results);
+	
+} else {
+	// Get user input from post, append %
+	$userinput = $_POST['term']."%";
+
+	// Query
+	$dbquery = pg_query_params($surestore_db, "select * from surecustomer where LOWER(surecustomer.custfirst) like LOWER($1) or LOWER(surecustomer.custlast) like LOWER($1) or LOWER(surecustomer.custbusiness) like LOWER($1)", array($userinput));
+
+	// Initialize ID variable
+	$id = 1;
+
+	while ($dbresult = pg_fetch_assoc($dbquery)) {
+
+		$entry = [];
+		$entry["id"] = $id;
+		$entry["text"] = "Customer: ".$dbresult["custfirst"]." ".$dbresult["custlast"]." Business: ".$dbresult["custbusiness"];
+		array_push($results, $entry);
+		$id++;
+	}
+
+	$data["results"] = $results;
+
+	// Echo back results
+	echo json_encode($data);
 }
 
-$data["results"] = $results;
-
-// Echo back results
-echo json_encode($data);
 
 // Close DB connection
 pg_close($surestore_db);
