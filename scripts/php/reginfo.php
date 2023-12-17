868b79a7cbfa0c6efe77449a980d04f42797d92c
tyler@shetrompf.com <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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>
2023-12-17 00:05:57 -0500

    adjusted query to remove surecustomer join

--- scripts/php/reginfo.php
+++ scripts/php/reginfo.php
@@ -8,7 +8,7 @@ $data = [];
 
 $orderid = $_POST["orderid"];
 
-$reginfoquery = pg_query_params($surestore_db, "select sureorders.orderid, sureorders.orderwh, sureorders.ordermil, sureorders.datein, sureorders.dateout, sureorders.weight, surecustomer.*, timezone('US/Eastern', date_trunc('minute', surehistory.histtime)) from sureorders left join surecustomer on surecustomer.custid = sureorders.ordercust left join surehistory on surehistory.historder = sureorders.orderid where sureorders.orderid=$1", array($orderid));
+$reginfoquery = pg_query_params($surestore_db, "select sureorders.orderid, sureorders.orderwh, sureorders.ordermil, sureorders.datein, sureorders.dateout, sureorders.weight, timezone('US/Eastern', date_trunc('minute', surehistory.histtime)) from sureorders left join surehistory on surehistory.historder = sureorders.orderid where sureorders.orderid=$1", array($orderid));
 
 $reginforesults = pg_fetch_assoc($reginfoquery);
 
