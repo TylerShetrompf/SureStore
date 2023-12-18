2c174a73764e5f58f8908dba60e06575ce3274af
tyler@shetrompf.com <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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>
2023-12-18 03:12:27 -0500

    moved collection of histtime to its own query to correct an issue where the oldest modtime was grabbed instead of the most recent

--- scripts/php/reginfo.php
+++ scripts/php/reginfo.php
@@ -8,9 +8,15 @@ $data = [];
 
 $orderid = $_POST["orderid"];
 
-$reginfoquery = pg_query_params($surestore_db, "select sureorders.orderid, sureorders.orderwh, sureorders.ordermil, sureorders.datein, sureorders.dateout, sureorders.weight, timezone('US/Eastern', date_trunc('minute', surehistory.histtime)) from sureorders left join surehistory on surehistory.historder = sureorders.orderid where sureorders.orderid=$1", array($orderid));
+$reginfoquery = pg_query_params($surestore_db, "select sureorders.orderid, sureorders.orderwh, sureorders.ordermil, sureorders.datein, sureorders.dateout, sureorders.weight from sureorders where sureorders.orderid=$1", array($orderid));
 
 $reginforesults = pg_fetch_assoc($reginfoquery);
+
+$lastmodquery = pg_query_params($surestore_db, "select timezone('US/Eastern', date_trunc('minute', histtime)) as histtime from surehistory where historder = $1 order by histtime desc limit 1", array($orderid));
+
+$lastmodresult = pg_fetch_assoc($lastmodquery);
+
+$reginforesults["histtime"] = $lastmodresult["histtime"];
 
 echo json_encode($reginforesults);
 
