dcc31743fd1241213d85e95f77950b3ced54bd2e
tyler@shetrompf.com <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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>
2023-12-18 09:23:13 -0500

    initial commit, functionally sound for order creation

---
+++ scripts/js/createorder.js
@@ -0,0 +1,52 @@
+// JavaScript Document to handle new orders
+$(document).ready(function (){
+	$("body").on("submit", "#neworderform", function (event){
+		event.preventDefault();
+		var formData ={
+			custbusiness: $('#businessinput').val(),
+			custtn : $('#cust-tn').val(),
+			custfirst: $('#custfirstinput').val(),
+			custlast: $('#custlastinput').val(),
+			custaddress: $('#custaddyinput').val(),
+			custcity: $('#custcityinput').val(),
+			custzip: $('#custzipinput').val(),
+			orderid: $('#reginput').val(),
+			orderwh: $('#regwhinput').val(),
+			datein: $('#regdateininput').val(),
+			weight: $('#regweightinput').val(),
+			custstate: $('#custstateinput').val(),
+			custcountry: $('#custcountryinput').val()
+		}
+		$.ajax({
+			url: "/scripts/php/createorder.php",
+			type: "POST",
+			data: formData,
+			dataType: "json",
+			encode: true
+		}).done(function (data) {
+			if (data["success"] == "false") {
+				$("#neworderdiv").prepend('<div class="alert alert-danger" role="alert"><h4 class="alert-heading">ERROR</h4><p>ORDER CREATION OR CUSTOMER CREATION HAS FAILED. YOUR CHANGES MAY NOT HAVE BEEN SAVED.</p><hr><p class="mb-0">PLEASE CONTACT SYSTEM ADMINISTRATOR.</p></div>');
+			} else {
+				$.get('/snippets/vaultinfo/vaultinfo.html', function(data) {
+					$("#appcontainer").html(data);
+				})
+
+				$.get('/snippets/vaultinfo/vaultinfoleft.html', function(data) {
+					$("#left").html(data);
+				}).done(function(){
+					initializeItemTable(formData["orderid"]);
+				})
+
+				$.get('/snippets/vaultinfo/vaultinfomiddle.php', function(data) {
+					$("#middle").html(data);
+				}).done(function(){
+					initializeSelect2();
+					fillreginfo(formData["orderid"]);
+					fillcustinfo(formData["orderid"]);
+					custsearch();
+				})
+			}
+		});
+		
+	});
+});
---
+++ scripts/php/createorder.php
@@ -0,0 +1,69 @@
+<?php
+
+// Include db connection
+include '/var/www/html/scripts/php/connectdb.php';
+
+$data = [];
+
+// Initialize customer vars
+$custbusiness = $_POST["custbusiness"];
+$custtn = $_POST["custtn"];
+$custtn = preg_replace("/[^0-9]/", "", $custtn );
+$custfirst = $_POST["custfirst"];
+$custlast = $_POST["custlast"];
+$custaddress = $_POST["custaddress"];
+$custcity = $_POST["custcity"];
+$custzip = $_POST["custzip"];
+$custstate = $_POST["custstate"];
+$custcountry = $_POST["custcountry"];
+
+// Initialize order vars
+$orderid = $_POST["orderid"];
+$orderwh = $_POST["orderwh"];
+$datein = $_POST["datein"];
+$weight = $_POST["weight"];
+
+// Userid from cookie
+$userid = $_COOKIE["userid"];
+
+// Cust query
+$custquery = pg_query_params($surestore_db, "insert into surecustomer(custbusiness, custtn, custfirst, custlast, custaddress, custcity, custzip, custstate, custcountry) values($1, $2, $3, $4, $5, $6, $7, $8, $9) returning custid", array($custbusiness, $custtn, $custfirst, $custlast, $custaddress, $custcity, $custzip, $custstate, $custcountry));
+
+// cust result
+$custresult = pg_fetch_assoc($custquery);
+
+// check if row created
+if (pg_affected_rows($custquery) == 0){
+	
+	$data["success"] = "false";
+	echo json_encode($data);
+	
+} else {
+	$data["custid"] = $custresult;
+	// order query
+	$orderquery = pg_query_params($surestore_db, "insert into sureorders(orderid, orderwh, datein, weight, ordercust) values($1, $2, $3, $4, $5)", array($orderid, $orderwh, $datein, $weight, $custresult["custid"]));
+	
+	// check if row created
+	if (pg_affected_rows($orderquery) == 0){
+		
+		$data["success"] = "false";
+		echo json_encode($data);
+		
+	} else {
+		
+		// cust hist
+		$custhisttext = $userid." created customer ".$custfirst." ".$custlast.".";
+		$custhistquery = pg_query_params($surestore_db, "insert into surehist(histdesc, historder) values($1, $2)", array($custhisttext, $orderid));
+		
+		// order hist
+		$orderhisttext = $userid." created order ".$orderid." "." with customer ".$custfirst." ".$custlast.".";
+		$custhistquery = pg_query_params($surestore_db, "insert into surehist(histdesc, historder) values($1, $2)", array($custhisttext, $orderid));
+		
+		$data["success"] = "true";
+		echo json_encode($data);
+		
+	}
+
+}
+
+pg_close($surestore_db);
