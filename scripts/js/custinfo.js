05064aea293db4a6fc7c7acb0cafc7fc30e84553
tyler@shetrompf.com <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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>
2023-12-18 08:26:04 -0500

    relocated tn masking to custsearch function so it will function on create order and view

--- scripts/js/custinfo.js
+++ scripts/js/custinfo.js
@@ -71,6 +71,57 @@ function custsearch() {
 		});
 		
 	});
+	
+	// Phone number masking
+	$('#cust-tn').keydown(function (e) {
+		var key = e.which || e.charCode || e.keyCode || 0;
+		$phone = $(this);
+
+		// Don't let them remove the starting '('
+		if ($phone.val().length === 1 && (key === 8 || key === 46)) {
+				$phone.val('('); 
+			return false;
+		} else if ($phone.val().charAt(0) !== '(') {
+			// Reset if they highlight and type over first char.
+			$phone.val('('+String.fromCharCode(e.keyCode)+''); 
+		}
+
+		// Auto-format- do not expose the mask as the user begins to type
+		if (key !== 8 && key !== 9) {
+			if ($phone.val().length === 4) {
+				$phone.val($phone.val() + ')');
+			}
+			if ($phone.val().length === 5) {
+				$phone.val($phone.val() + ' ');
+			}			
+			if ($phone.val().length === 9) {
+				$phone.val($phone.val() + '-');
+			}
+		}
+
+		// Allow numeric (and tab, backspace, delete) keys only
+		return (key == 8 || 
+				key == 9 ||
+				key == 46 ||
+				(key >= 48 && key <= 57) ||
+				(key >= 96 && key <= 105));
+		
+	}).bind('focus click', function () {
+		$phone = $(this);
+
+		if ($phone.val().length === 0) {
+			$phone.val('(');
+		} else {
+			var val = $phone.val();
+			$phone.val('').val(val); // Ensure cursor remains at the end
+		}
+	}).blur(function () {
+		$phone = $(this);
+
+		if ($phone.val() === '(') {
+			$phone.val('');
+		}
+	});
 }
 
 $(document).ready(function (){
@@ -121,66 +172,7 @@ $(document).ready(function (){
 				})
 			}
 		});
-	});
-	
-	// Phone number masking
-	$('#cust-tn')
-
-	.keydown(function (e) {
-		var key = e.which || e.charCode || e.keyCode || 0;
-		$phone = $(this);
-
-    // Don't let them remove the starting '('
-    if ($phone.val().length === 1 && (key === 8 || key === 46)) {
-			$phone.val('('); 
-      return false;
-		} 
-    // Reset if they highlight and type over first char.
-    else if ($phone.val().charAt(0) !== '(') {
-			$phone.val('('+String.fromCharCode(e.keyCode)+''); 
-		}
-
-		// Auto-format- do not expose the mask as the user begins to type
-		if (key !== 8 && key !== 9) {
-			if ($phone.val().length === 4) {
-				$phone.val($phone.val() + ')');
-			}
-			if ($phone.val().length === 5) {
-				$phone.val($phone.val() + ' ');
-			}			
-			if ($phone.val().length === 9) {
-				$phone.val($phone.val() + '-');
-			}
-		}
-
-		// Allow numeric (and tab, backspace, delete) keys only
-		return (key == 8 || 
-				key == 9 ||
-				key == 46 ||
-				(key >= 48 && key <= 57) ||
-				(key >= 96 && key <= 105));	
-	})
-	
-	.bind('focus click', function () {
-		$phone = $(this);
-		
-		if ($phone.val().length === 0) {
-			$phone.val('(');
-		}
-		else {
-			var val = $phone.val();
-			$phone.val('').val(val); // Ensure cursor remains at the end
-		}
-	})
-	
-	.blur(function () {
-		$phone = $(this);
-		
-		if ($phone.val() === '(') {
-			$phone.val('');
-		}
-	});
-	
+	});	
 });
 
 function fillcustinfo(orderid){
