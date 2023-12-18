16a6f46ddad57e452fd892a3a4dd7942e3c96e86
tyler@shetrompf.com <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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>
2023-12-17 21:56:32 -0500

    added closedb

--- scripts/php/logout.php
+++ scripts/php/logout.php
@@ -9,3 +9,6 @@ $userid = $_COOKIE["userid"];
 
 $newsessionid = password_hash(($userid.time()), PASSWORD_DEFAULT);
 $logoutquery = pg_query_params($surestore_db, "update sureusers set sessionid = $1 where LOWER(userid) = LOWER($2)", array($newsessionid, $userid));
+
+// Close DB connection
+pg_close($surestore_db);
