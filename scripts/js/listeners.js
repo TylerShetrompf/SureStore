efd375da7de27b926f854cf9898119a8a81c4aae
tyler@shetrompf.com <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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>
2023-12-18 14:29:46 -0500

    moved listeners for milcheck and regform submissions to listeners.js

--- scripts/js/listeners.js
+++ scripts/js/listeners.js
@@ -295,4 +295,67 @@ $(document).ready(function () {
 	}); // end of listener for navbar home button
 	
 	
+	// Listener for military check switch
+	$("body").on("click", "#milcheck", function (){
+		if ($("#milcheck").attr("checked") == "checked"){
+			$("#milcheck").attr("checked", false);
+		} else if ($("#milcheck").attr("checked") == undefined){
+			$("#milcheck").attr("checked", true);
+		}
+	}); // end of listener for military check switch
+	
+	
+	// Listener for reg update form submits
+	$('body').on("submit", "#reginfoform", function (event){
+		event.preventDefault();
+		
+		var regformdata ={
+			oldorderid: $("#hiddenorderid").val(),
+			orderid: $('#reginput').val(),
+			orderwh: $('#regwhinput').val(),
+			datein: $('#regdateininput').val(),
+			weight: $('#regweightinput').val()
+		}
+		
+		if ($('#regdateoutinput').val() != ""){
+			regformdata["dateout"] = $('#regdateoutinput').val();
+		}
+		
+		// Check if ordermil box is ticked and if so add to regformdata
+		if ($("#milcheck").attr("checked") == "checked"){
+			regformdata["ordermil"] = true;
+		}
+		
+		$.ajax({
+			url: "/scripts/php/updatereginfo.php",
+			type: "POST",
+			data: regformdata,
+			dataType: "json",
+			encode: true
+		}).done(function (data){
+			if (data["success"] == "false"){
+				$("#reginfodiv").append('<div class="alert alert-danger" role="alert">Update failed. Please contact system administrator.</div>');
+			} else {
+				$.get('/snippets/vaultinfo/vaultinfo.html', function(data) {
+					$("#appcontainer").html(data);
+				}).done(function (){
+					$.get('/snippets/vaultinfo/vaultinfoleft.html', function(data) {
+						$("#left").html(data);
+					}).done(function(){
+						$.get('/snippets/vaultinfo/vaultinfomiddle.php', function(data) {
+							$("#middle").html(data);
+						}).done(function(){
+							initorderfull(regformdata["orderid"]);
+						})
+					})
+				})
+			}
+		});
+		
+	}); // end of listener for reg update form submits
+	
+	
+	
+	
+	
 }) // End of document.ready
