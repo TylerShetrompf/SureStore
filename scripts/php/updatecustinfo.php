5fc82d41d090a343519a2026b0eea7e8a2572b33
tyler@shetrompf.com <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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>
2023-12-18 13:52:06 -0500

    added var declarations for cust state and cust country, as well as updated query to update those values

--- scripts/php/updatecustinfo.php
+++ scripts/php/updatecustinfo.php
@@ -15,6 +15,8 @@ $custaddress = $_POST["custaddress"];
 $custcity = $_POST["custcity"];
 $custzip = $_POST["custzip"];
 $orderid = $_POST["orderid"];
+$custstate = $_POST["custstate"];
+$custcountry = $_POST["custcountry"];
 
 // initialize userid variable from cookie
 $userid = $_COOKIE["userid"];
@@ -22,7 +24,7 @@ $userid = $_COOKIE["userid"];
 // check if custid was posted, if not make a new customer
 if ($_POST["custid"]) {
 	$custid = $_POST["custid"];
-	$custupquery = pg_query_params($surestore_db, "update surecustomer set custbusiness = $1, custtn = $2, custfirst = $3, custlast = $4, custaddress = $5, custcity = $6, custzip = $7 where custid = $8", array($custbus, $custtn, $custfirst, $custlast, $custaddress, $custcity, $custzip, $custid));
+	$custupquery = pg_query_params($surestore_db, "update surecustomer set custbusiness = $1, custtn = $2, custfirst = $3, custlast = $4, custaddress = $5, custcity = $6, custzip = $7, custstate = $8, custcountry = $9 where custid = $10", array($custbus, $custtn, $custfirst, $custlast, $custaddress, $custcity, $custzip, $custstate, $custcountry, $custid));
 	
 	if (pg_affected_rows($custupquery) == 0){
 		$data["success"] = "false";
@@ -32,17 +34,23 @@ if ($_POST["custid"]) {
 		$histquery = pg_query_params($surestore_db, "insert into surehistory(historder, histdesc) values($1, $2)", array($orderid,$updatetext));
 	}
 } else {
-	$custcreatequery = pg_query_params($surestore_db, "insert into surecustomer(custbusiness, custtn, custfirst, custlast, custaddress, custcity, custzip) values($1, $2, $3, $4, $5, $6, $7) returning custid", array($custbus, $custtn, $custfirst, $custlast, $custaddress, $custcity, $custzip));
+	$custcreatequery = pg_query_params($surestore_db, "insert into surecustomer(custbusiness, custtn, custfirst, custlast, custaddress, custcity, custzip, custstate, custcountry) values($1, $2, $3, $4, $5, $6, $7, $8, $9) returning custid", array($custbus, $custtn, $custfirst, $custlast, $custaddress, $custcity, $custzip, $custstate, $custcountry));
 	
 	if(pg_affected_rows($custcreatequery) == 0){
 		$data["success"] = "false";
+	} else {
+		$newcustrow = pg_fetch_assoc($custcreatequery);
+		$custid = $newcustrow[0];
+		$ordercustquery = pg_query_params($surestore_db, "update sureorders set ordercust = $1 where orderid = $2", array($custid, $orderid));
+		if(pg_affected_rows($ordercustquery) == 0){
+			$data["success"] = "false";
+		} else {
+			// Log in surehistory
+			$updatetext = $userid." updated customer information for order ".$orderid." to ".$custfirst." ".$custlast.".";
+			$histquery = pg_query_params($surestore_db, "insert into surehistory(historder, histdesc) values($1, $2)", array($orderid,$updatetext));
+		}
 	}
-	$newcustrow = pg_fetch_row($custcreatequery);
-	$custid = $newcustrow[0];
-	$ordercustquery = pg_query_params($surestore_db, "update sureorders set ordercust = $1 where orderid = $2", array($custid, $orderid));
-	if(pg_affected_rows($ordercustquery) == 0){
-		$data["success"] = "false";
-	}
+
 }
 echo json_encode($data);
 
