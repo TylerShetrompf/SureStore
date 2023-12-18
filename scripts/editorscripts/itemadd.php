f0d102d8fb5b335391bda76f15f18b2d5ad611d9
tyler@shetrompf.com <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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>
2023-12-18 03:43:41 -0500

    added query to log adds to surehistory

--- scripts/editorscripts/itemadd.php
+++ scripts/editorscripts/itemadd.php
@@ -3,10 +3,13 @@
 // Include db connection
 include '/var/www/html/scripts/php/connectdb.php';
 
+$data = [];
+
 $itemdesc = $_POST["itemdesc"];
 $itemvault = $_POST["itemvault"];
 $itemloose = $_POST["itemloose"];
 $itemorder = $_POST["itemorder"];
+$userid = $_COOKIE["userid"];
 
 // Turn vaulter name into vaulterid 
 $itemvaultername = $_POST["itemvaulter"];
@@ -17,16 +20,39 @@ $vaulteridquery = pg_query_params($surestore_db, "select * from surevaulters whe
 $vaulteridresult = pg_fetch_assoc($vaulteridquery);
 $itemvaulter = $vaulteridresult["vaulterid"];
 
-if ($itemvault){
+if ($itemvault && !$itemloose){
 	$itemcreatequery = pg_query_params($surestore_db, "INSERT INTO sureitems (itemdesc, itemvault, itemorder, itemvaulter) VALUES ($1, $2, $3, $4) RETURNING *;", array($itemdesc, $itemvault, $itemorder, $itemvaulter));
 	$createqueryresult = pg_fetch_assoc($itemcreatequery);
 	$createqueryresult["itemvaulter"] = $itemvaultername;
-	echo json_encode($createqueryresult);
+	
+	
+	if(pg_affected_rows($itemcreatequery) == 0){
+		$data["errors"] = "error here";
+		echo json_encode($data);
+	} else {
+		// Log in surehistory
+		$updatetext = $userid." Added item ".$createqueryresult["itemid"]." in order ".$itemorder.".";
+		$histquery = pg_query_params($surestore_db, "insert into surehistory(historder, histdesc) values($1, $2)", array($itemorder,$updatetext));
+		echo json_encode($createqueryresult);
+	}
 }
 
-if ($itemloose){
+if ($itemloose && !$itemvault){
 	$itemcreatequery = pg_query_params($surestore_db, "INSERT INTO sureitems (itemdesc, itemloose, itemorder, itemvaulter) VALUES ($1, $2, $3, $4) RETURNING *;", array($itemdesc, $itemloose, $itemorder, $itemvaulter));
 	$createqueryresult = pg_fetch_assoc($itemcreatequery);
 	$createqueryresult["itemvaulter"] = $itemvaultername;
-	echo json_encode($createqueryresult);
+	
+	if(pg_affected_rows($itemcreatequery) == 0){
+		$data["errors"] = "error here";
+		echo json_encode($data);
+	} else {
+		// Log in surehistory
+		$updatetext = $userid." Added item ".$createqueryresult["itemid"]." in order ".$itemorder.".";
+		$histquery = pg_query_params($surestore_db, "insert into surehistory(historder, histdesc) values($1, $2)", array($itemorder,$updatetext));
+		echo json_encode($createqueryresult);
+	}
+	
 }
+
+// Close DB connection
+pg_close($surestore_db);
