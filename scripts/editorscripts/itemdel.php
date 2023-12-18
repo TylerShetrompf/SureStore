a2074640ab6c65e38584cc7c5c742368c5b29f09
tyler@shetrompf.com <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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>
2023-12-18 03:47:43 -0500

    added query to log deletes to surehistory

--- scripts/editorscripts/itemdel.php
+++ scripts/editorscripts/itemdel.php
@@ -4,8 +4,13 @@
 include '/var/www/html/scripts/php/connectdb.php';
 
 $itemid = $_POST["itemid"];
+$itemorder = $_POST["itemorder"];
+$userid = $_COOKIE["userid"];
 
 $deletequery = pg_query_params($surestore_db, "DELETE FROM sureitems where itemid = $1", array($itemid));
 $deletequeryresult = pg_fetch_assoc($deletequery);
+
+$updatetext = $userid." deleted item ".$itemid." from order ".$orderid.".";
+$histquery = pg_query_params($surestore_db, "insert into surehistory(historder, histdesc) values($1, $2)", array($itemorder,$updatetext));
 
 echo json_encode($deletequeryresult);
