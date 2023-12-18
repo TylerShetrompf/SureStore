0bf7ec0c290df7fdd6a03e10cc8a9f7673d5bb68
tyler@shetrompf.com <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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>
2023-12-18 14:32:25 -0500

    moved listeners for main registration button and registration form submissions to listeners.js

--- scripts/js/listeners.js
+++ scripts/js/listeners.js
@@ -355,6 +355,58 @@ $(document).ready(function () {
 	}); // end of listener for reg update form submits
 	
 	
+	// Listener for main menu register button
+	$("#regButton").click(function () {
+		
+		$.get('/snippets/registration.html', function(data) {
+			$("#middle").html(data);
+		})
+	}); // end of listener for main menu register button
+	
+	// Listener for registerform submissions
+	$("body").on("submit", "#RegisterForm", function(event) {
+		
+		event.preventDefault();
+		
+		var formData ={
+			firstname: $("#firstname").val(),
+			lastname: $("#lastname").val(),
+			email: $("#email").val(),
+			password: $("#password").val(),
+		}
+		
+		$.ajax({
+			type: "POST",
+			url: "/scripts/php/register.php",
+			data: formData,
+			dataType: "json",
+			encode: true,
+		}).done(function (data) {
+
+			if (!data.success) {
+				
+				if (data.errors.password) {
+					$("#FormPassword").addClass("has-error");
+					$("#FormPassword").append(
+						'<div class="help-block">' + data.errors.password + "</div>"
+					);
+				}
+				if (data.errors.username) {
+					$("#FormPassword").addClass("has-error");
+					$("#FormPassword").append(
+						'<div class="help-block">' + data.errors.username + "</div>"
+					);
+				}
+			} else {
+				$.get('/snippets/registrationsuccess.html', function(newHTMLdata) {
+					$("#middle").html(newHTMLdata);
+				})
+			}
+			
+		});
+	}); // end of listener for registerform submissions
+	
+	
 	
 	
 	
