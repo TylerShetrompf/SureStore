2433d595e34b1a0842b0ba3c322d613d8375a9eb
tyler@shetrompf.com <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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>
2023-12-21 16:56:58 -0500

    incomplete, still working on pdf generation

--- scripts/php/pdfgen.php
+++ scripts/php/pdfgen.php
@@ -2,28 +2,37 @@
 require('/var/www/html/scripts/php/fpdf.php');
 
 // get variables from post
-$orderid = $_POST["orderid"];
-$dataurl = $_POST['dataurl'];
+$orderid = "696966";
+// $dataurl = $_POST['dataurl'];
 
 // Set path and name for qr code
-$imgname = '../../images/QR/'.$orderid.".png";
+$imgname = '../../QR/'.$orderid.".png";
 
-$dataPieces = explode(',', $dataurl);
-$encodedImg = $dataPieces[1];
-
-$decodedImg = base64_decode($encodedImg);
-
-if( $decodedImg!==false ) {
+if (file_exists($imgname)) {
 	
-	if( file_put_contents($imgname,$decodedImg)!==false ) {
-		
-		$pdfname = $orderid.".pdf";
-		$pdfpath = '../../images/QR/'.$pdfname;
-		$pdf = new FPDF();
-        $pdf->AddPage();
-        $pdf->Image($imgname);
-        $pdf->Output('F', $pdfpath);
-		
-		echo json_encode($pdfname);
+	$pdfname = $orderid.".pdf";
+	$pdfpath = '../../QR/'.$pdfname;
+	$pdf = new FPDF('P','in','Letter');
+	$pdf->AddPage();
+	$pdf->SetFont('Arial','B',15);
+	$pdf->Image($imgname);
+	$pdf->Text(4,1,'Order: '.$orderid);
+	$pdf->Output('F', $pdfpath);
+	echo json_encode($pdfname);
+} /* else {
+	$dataPieces = explode(',', $dataurl);
+	$encodedImg = $dataPieces[1];
+
+	$decodedImg = base64_decode($encodedImg);
+
+	if( $decodedImg!==false ) {
+	
+		if( file_put_contents($imgname,$decodedImg)!==false ) {
+			$pdfname = $orderid.".pdf";
+			$pdfpath = '../../QR/'.$pdfname;
+			$pdf = new FPDF();
+
+		}
 	}
 }
+*/
