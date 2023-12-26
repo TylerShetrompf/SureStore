9211222a1afa5f9729391214d427c92051a69352
tyler@shetrompf.com <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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>
2023-12-26 02:59:43 -0500

    updated page to only display maintenance and admin buttons if user possesses the correct acl

--- snippets/mainmenu.php
+++ snippets/mainmenu.php
@@ -1,6 +1,17 @@
 <?php
 // PHP to check session
-include '/var/www/html/scripts/php/reverifysession.php';
+//include '/var/www/html/scripts/php/reverifysession.php';
+
+// Include db connection
+include '/var/www/html/scripts/php/connectdb.php';
+
+// Get userid from cookie
+$userid = $_COOKIE['userid'];
+
+// Check user privelege level
+$userquery = pg_query_params($surestore_db, 'select useracl from sureusers where LOWER(userid) = LOWER($1)', array($userid));
+
+$userresult = pg_fetch_assoc($userquery);
 
 echo(
 		
@@ -22,16 +33,27 @@ echo(
 			<button type="button" class="btn btn-success btn-lg btn-block">Scan</button>
 		</div>'
 );
+
+if ($userresult["useracl"] >= "2") {
+	
 echo ('	
 		<div class="d-grid gap-2 my-2" id="maintButton">
 			<button type="button" class="btn btn-warning btn-lg btn-block">Maintenance</button>
 		</div>'
 );
+	
+}
+
+if ($userresult["useracl"] == "3") {
+	
 echo ('	
 		<div class="d-grid gap-2 my-2" id="adminButton">
 			<button type="button" class="btn btn-danger btn-lg btn-block">Admin</button>
 		</div>'
 );
+	
+}
+
 
 echo ('	
 	</div>
