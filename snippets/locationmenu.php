d7c105b4f8b7e249eba3b3215908a1bc6f893a13
tyler@shetrompf.com <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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>
2023-12-27 16:48:22 -0500

    initial commit, complete snippet for location menu presented when a location qr is scanned

---
+++ snippets/locationmenu.php
@@ -0,0 +1,13 @@
+<?php
+// PHP to check session
+include '/var/www/html/scripts/php/reverifysession.php';
+
+echo('
+
+<div class="bg-light my-2 border rounded">
+	<h6 class="text-center">Items in Vault</h6>
+	<table id="locitemtable" class="display" style="width:100%"></table>
+</div>
+
+');
+?>
