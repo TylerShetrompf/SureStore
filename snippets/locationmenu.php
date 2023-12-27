4cec03d46347f5fdc4bad9756911805268ff04ed
tyler@shetrompf.com <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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>
2023-12-27 16:50:47 -0500

    added missing id to table header and removed extraneous placeholder text

--- snippets/locationmenu.php
+++ snippets/locationmenu.php
@@ -5,7 +5,7 @@ include '/var/www/html/scripts/php/reverifysession.php';
 echo('
 
 <div class="bg-light my-2 border rounded">
-	<h6 class="text-center">Items in Vault</h6>
+	<h6 class="text-center" id="locitemheading"></h6>
 	<table id="locitemtable" class="display" style="width:100%"></table>
 </div>
 
