81b822b50b2dfb6188b495c53026a83d6e73b68b
tyler@shetrompf.com <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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>
2023-12-29 12:19:46 -0500

    initial commit, complete snippet for reset success page

---
+++ snippets/resetsuccess.php
@@ -0,0 +1,8 @@
+<div class="bg-light rounded-3">
+	<h1>Password Reset</h1>
+	<p>Please check your email for a link to reset your password.</p>
+</div>
+
+<div class="d-grid gap-2 my-2">
+	<a href="https://surestore.store" class="btn btn-success btn-large btn-block">Return to login</a>
+</div>
