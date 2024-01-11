a5f69a8082f621e9ee7df2b419a9255504fd57f4
tyler@shetrompf.com <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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>
2024-01-11 15:05:48 -0500

    initial commit, complete PHP script to return data for custorderstab

---
+++ scripts/php/custorders.php
@@ -0,0 +1,37 @@
+<?php
+// PHP to check session
+include '/var/www/html/scripts/php/reverifysession.php';
+// Include db connection
+include '/var/www/html/scripts/php/connectdb.php';
+
+// initialize custorders array
+$custorders = [];
+
+// get orderid from post
+$orderid = $_POST["orderid"];
+
+// query to Get custid from order
+$custidquery = pg_query_params($surestore_db, "select ordercust from sureorders where orderid = $1", array($orderid));
+
+// cust id to var
+$custidres = pg_fetch_assoc($custidquery);
+$custid = $custidres["ordercust"];
+
+// Get customers other active orders
+$ordersquery = pg_query_params($surestore_db, "select orderid, orderwh, datein from sureorders where ordercust = $1 AND dateout is NULL", array($custid));
+
+//iterate through results, push entries to custorders array
+while ($ordersqueryresult = pg_fetch_assoc($ordersquery)) {
+	$entry = [];
+	$entry ["orderid"] = $ordersqueryresult["orderid"];
+	$entry ["orderwh"] = $ordersqueryresult["orderwh"];
+	$entry ["datein"] = $ordersqueryresult["datein"];
+	
+	array_push($custorders, $entry);
+}
+
+// Echo back to ajax
+echo json_encode($custorders);
+
+// Close db connection
+pg_close($surestore_db);
