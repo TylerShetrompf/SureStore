6d8e7420bc2d0cf93eb6b397ed27f0a4604cc12b
tyler@shetrompf.com <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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>
2024-01-16 00:35:09 -0500

    initial commit - script for daily expired sit report

---
+++ scripts/reports/dailyexpired.php
@@ -0,0 +1,39 @@
+<?php
+// include PHPMailer
+require_once('/var/www/html/scripts/phpmailer/PHPMailer.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Include db connection
+include '/var/www/html/scripts/php/connectdb.php';
+
+$exsitquery = pg_query($surestore_db, "select orderid, sitex, sitnum from sureorders where sitex > CURRENT_DATE");
+
+$opencsv = fopen('../../temp/tempsitex.csv', 'w');
+
+fputcsv($opencsv, array("OrderID","Expiration","SIT Number"));
+
+while ($row = pg_fetch_row($exsitquery)){
+	fputcsv($opencsv, $row);
+}
+
+fclose($opencsv);
+
+$email = new PHPMailer();
+$email->isSendmail();
+$email->SetFrom('admin@surestore.store'); //Name is optional
+$email->Subject   = 'Expired SIT Report';
+$email->Body      = "See attached for report.";
+$email->AddAddress( 'tyler@shetrompf.com' );
+
+$file_to_attach = '../../temp/tempsitex.csv';
+
+$email->AddAttachment( $file_to_attach , 'report.csv' );
+
+return $email->Send();
+
+unlink($opencsv);
+
+// Close DB connection
+pg_close($surestore_db);
