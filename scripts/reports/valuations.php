d8ca3ea35c926f964a56e56113df268fce09ba11
tyler@shetrompf.com <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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>
2024-01-16 19:04:27 -0500

    updated script to generate a better report

--- scripts/reports/valuations.php
+++ scripts/reports/valuations.php
@@ -1,10 +1,10 @@
 <?php
 // PHP to check session
-//include '/var/www/html/scripts/php/reverifysession.php';
+include '/var/www/html/scripts/php/reverifysession.php';
 // Include db connection
 include '/var/www/html/scripts/php/connectdb.php';
 
-$cols = array("OrderID","Weight","Valuation");
+$cols = array("OrderID","Order Type","Weight", "Valuation Type","Valuation");
 
 $opencsv = fopen('../../temp/valuations.csv', 'w');
 
@@ -16,17 +16,27 @@ while ($whrow = pg_fetch_assoc($whquery)){
 	
 	fputcsv($opencsv, $cols);
 	
-	$queryorders = pg_query_params($surestore_db, "select distinct(sureorders.orderid), sureorders.weight, sureorders.orderval from sureorders left join sureitems on sureitems.itemorder = sureorders.orderid where sureitems.dateout is NULL AND sureorders.orderwh = $1", array($whrow["whid"]));
+	$queryorders = pg_query_params($surestore_db, "select distinct(sureorders.orderid), sureorders.ordertype, sureorders.weight, sureorders.valtype, sureorders.orderval::numeric::float from sureorders left join sureitems on sureitems.itemorder = sureorders.orderid where sureitems.dateout is NULL AND sureorders.orderwh = $1", array($whrow["whid"]));
+	
+	$total = 0.0;
 	
 	while ($row = pg_fetch_assoc($queryorders)){
-		fputcsv($opencsv, array($row["orderid"], $row["weight"], $row["orderval"]));
-	}
+		$valtype;
+		if ($row["valtype"] == "60l"){
+			$valtype = "$0.60/lb";
+		}
+		if ($row["valtype"] == "frc"){
+			$valtype = "Full Replacement Charge";
+		}
+		if ($row["valtype"] == "oth"){
+			$valtype = "OTHER";
+		}
+		
+		fputcsv($opencsv, array($row["orderid"], $row["ordertype"], $row["weight"], $valtype, "$".$row["orderval"]));
+		$total += $row["orderval"];
+	}	
 	
-	$qtotal = pg_query_params($surestore_db, "select SUM(sureorders.orderval) from sureorders left join sureitems on sureitems.itemorder = sureorders.orderid where sureitems.dateout is NULL AND sureorders.orderwh = $1", array($whrow["whid"]));
-	
-	$totalval = pg_fetch_assoc($qtotal);
-
-	$totalrow = array("", "Total:", $totalval["sum"]);
+	$totalrow = array("", "", "", "Total:", "$".$total);
 	fputcsv($opencsv, $totalrow);
 	fputcsv($opencsv, array(""));
 	fputcsv($opencsv, array(""));
