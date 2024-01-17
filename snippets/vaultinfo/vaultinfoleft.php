ee87a0dca8a8cc5ff0d32022478ee7d9b6eb7062
tyler@shetrompf.com <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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>
2024-01-16 19:05:49 -0500

    added button and modal for show open vaults

--- snippets/vaultinfo/vaultinfoleft.php
+++ snippets/vaultinfo/vaultinfoleft.php
@@ -6,5 +6,36 @@ echo('
 	<h6 class="text-center">Order Items</h6>
 	<table id="iteminfo" class="display" style="width:100%"></table>
 </div>
+
+<!-- Show openvaults button -->
+<div class="d-grid gap-2 my-2">
+	<button type="button" id="showopenvaultsbtn" class="btn btn-secondary btn-large btn-block" data-bs-toggle="modal" data-bs-target="#openvaultsModal">Show Open Vaults</button>
+</div>
+
+<!-- openvaults Modal -->
+<div class="modal" id="openvaultsModal">
+	<div class="bg-light px-2 my-2 border rounded" id="openvaultsmodaldiv">
+		<div class="modal-dialog">
+			<div class="modal-content">
+
+				<!-- Modal Header -->
+				<div class="modal-header">
+					<h4 class="modal-title" id="openvaultsheading"></h4>
+					<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+				</div>
+
+				<!-- Modal Body -->
+				<div class="modal-body">
+					<table id="openvaultstab" class="display" style="width:100%"></table>
+				</div>
+				<!-- Modal Footer -->
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
 ');
 ?>
