9188cc0f5e1f8e1bafe7643d4c4c63139d0a6053
tyler@shetrompf.com <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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>
2024-01-18 12:45:50 -0500

    changed pdframe from iframe to embed

--- snippets/vaultinfo/vaultinforight.php
+++ snippets/vaultinfo/vaultinforight.php
@@ -5,7 +5,7 @@ include '/var/www/html/scripts/php/reverifysession.php';
 echo('
 
 <div class="bg-light px-2 my-2 border rounded" id="pdfdiv">
-	<iframe id="pdfframe" class="my-2" frameborder="0"></iframe>
+	<embed id="pdfframe" class="my-2" frameborder="0" type="application/pdf"></embed>
 	<div class="d-grid gap-2 my-2">
 		<input type="button" id="printbtn" value="Print Locator Sheet" class="btn btn-info btn-large btn-block">
 	</div>
